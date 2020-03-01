e69670be9523ca79ae8f5ffae0ec0ae87c9ef488
rami2076 <sakusennx@yahoo.co.jp>
2020-03-01 10:54:29 +0900

    init BorderPane

---
+++ src/main/java/org/example/fxpattern/gui/base/Base.java
@@ -0,0 +1,64 @@
+package org.example.fxpattern.gui.base;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.Root;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Base  implements Initializable {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private void ui1(ActionEvent event) {
+        loadUi("ui1");
+    }
+
+    @FXML
+    private void ui2(ActionEvent event) {
+        loadUi("ui2");
+
+    }
+
+    @FXML
+    private void ui3(ActionEvent event) {
+        loadUi("ui3");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    private void loadUi(String str) {
+
+        Parent root = null;
+        String pathString = format(str);
+        URL location = getClass().getResource(pathString);
+        try {
+            root = FXMLLoader.load(location);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        borderPane.setCenter(root);
+
+    }
+
+    private String format(String str) {
+        return String.format("%s/%s.fxml", str, StringUtils.capitalize(str));
+    }
+
+
+}
